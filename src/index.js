5fec81bfcc47d5db48a4ff80409a07842d55e36f
SnowDrive <luc.moralesr@gmail.com>
2022-05-05 05:07:43 -0400

    fix: Create clients Map for ws server

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 8000
 export const app = express()
 export const server = createServer(app)
 export const wsServer = new Server({ noServer: true })
+export const clients = new Map()
 
 app.use(express.json())
 app.use(cors())
